003f2351117e27fbab160f7e10f9c0309f0d76fe
OhSeungWan <face4717@gmail.com>
2024-03-27 20:13:13 +0900

    feat: react-router lib 추가

---
+++ src/shared/lib/react-router/config.ts
@@ -0,0 +1,23 @@
+// import { SlugPageParams, UsernamePageParams } from './react-router.types'
+
+export const pathKeys = {
+  root: '/',
+  challenge() {
+    return pathKeys.root.concat('challenge/')
+  },
+  mypage() {
+    return pathKeys.root.concat('my-page/')
+  },
+  write() {
+    return pathKeys.root.concat('write/')
+  },
+  feed() {
+    return pathKeys.root.concat('feed/')
+  },
+  home() {
+    return pathKeys.root
+  },
+  page404() {
+    return pathKeys.root.concat('404/')
+  },
+}
