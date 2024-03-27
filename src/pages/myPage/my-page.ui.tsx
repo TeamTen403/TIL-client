802fc3935fd5a7d268ddd2656b4d7fb0f380556c
OhSeungWan <face4717@gmail.com>
2024-03-27 22:23:55 +0900

    fix(my-page): add widgets

--- src/pages/myPage/my-page.ui.tsx
+++ src/pages/myPage/my-page.ui.tsx
@@ -1,3 +1,15 @@
+import { MainLayoutHeader } from '@/shared/ui'
+import { RecordsCalendar } from '@/widgets/records-calendar'
+import { TilThumbnailList } from '@/widgets/til-thumbnail-list'
+import { ViewerRecords } from '@/widgets/viewer-records'
+
 export function MyPage() {
-  return <div className="w-full">MyPage</div>
+  return (
+    <div className="w-full">
+      <MainLayoutHeader title="마이페이지" />
+      <ViewerRecords />
+      <RecordsCalendar />
+      <TilThumbnailList />
+    </div>
+  )
 }
