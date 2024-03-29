0378cb744937382a4c985ddf087f3f5a99c46fcb
OhSeungWan <face4717@gmail.com>
2024-03-29 13:16:41 +0900

    fix(records-calendar): style

--- src/widgets/records-calendar/records-calendar.ui.tsx
+++ src/widgets/records-calendar/records-calendar.ui.tsx
@@ -1,10 +1,10 @@
 import Calendar from 'react-calendar'
-import 'react-calendar/dist/Calendar.css'
+import './calendar.css'
 export function RecordsCalendar() {
   return (
     <div className="flex w-full flex-col gap-8 px-24 py-16">
-      <div className="text-18 font-[700]">TIL 켈린더</div>
-      <div className="flex w-full justify-center">
+      <div className="text-18 font-[700] leading-25 text-[#4E4E4E]">켈린더</div>
+      <div className="flex w-full justify-center rounded-[8px] bg-white px-24 py-16 shadow-[0px_0px_4px_0px_rgba(217,217,217,0.7)]">
         <Calendar next2Label={null} prev2Label={null} />
       </div>
     </div>
